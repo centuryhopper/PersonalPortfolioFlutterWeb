734d5eb23f6ab40b28b2443d6e48c0107dcd6be0
Leo Zhang <leotheasianlion@gmail.com>
2022-05-20 11:27:48 -0400

    added url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:personal_portfolio_flutter/Pages/PersonalPortfolio.dart';
-
+import 'package:url_strategy/url_strategy.dart';
 
 /// variables to initialize before running the app should be included in this method
 void initialize() {}
 
 Future<void> main() async {
+  setPathUrlStrategy();
   // initialize();
   // print(RegExp('^.*\n').stringMatch(newWebSiteContent));
   WidgetsFlutterBinding.ensureInitialized();
